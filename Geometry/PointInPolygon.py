30f2b74ffda22ef818000e9d5cd105ff7f147c92
kh277 <jkhcoding@naver.com>
2026-04-02 17:24:43 +0900

    (v1.03) PointInPolygon 알고리즘 예외처리 추가

--- Geometry/PointInPolygon.py
+++ Geometry/PointInPolygon.py
@@ -5,28 +5,40 @@
 여기서 hull은 반드시 볼록 다각형이어야 하며, 시계/반시계 방향으로 정렬되어 있어야 함.
 
 checkInner() : 다각형 hull 내부에 point가 존재하는지 여부 판정, O(logN)
+주석처리된 부분은 다각형 변 위에 존재하는 점을 제외시켜야 하는 경우의 코드임.
 '''
 
 def CCW(A, B, C):
     return (B[0]-A[0]) * (C[1]-A[1]) - (B[1]-A[1]) * (C[0]-A[0])
 
 
-def checkInner(point, hull):
-    # 1번과 n-1번 반직선 사이에 점이 존재하지 않는 경우
-    if CCW(hull[0], hull[-1], point) > 0:
-        return False
+def onLine(A, B, point):
+    return min(A[0], B[0]) <= point[0] <= max(A[0], B[0]) and min(A[1], B[1]) <= point[1] <= max(A[1], B[1])
+
+
+def checkInner(hull, point):
+    # 초기 선분 체크
+    # if CCW(hull[0], hull[1], point) <= 0:
     if CCW(hull[0], hull[1], point) < 0:
         return False
+    # if CCW(hull[0], hull[-1], point) >= 0:
+    if CCW(hull[0], hull[-1], point) > 0:
+        return False
 
-    # 이분 탐색으로 두 직선 구하기
+    # 이분 탐색으로 점이 속한 삼각형 찾기
     left = 1
     right = len(hull) - 1
     while left < right:
-        mid = (left + right) // 2
-        if CCW(hull[0], hull[mid], point) > 0:
-            left = mid + 1
+        mid = (left + right + 1) >> 1
+        if CCW(hull[0], hull[mid], point) >= 0:
+            left = mid
         else:
-            right = mid
+            right = mid - 1
 
-    # 최종적으로 남은 left와 left-1 사이에 점이 있는지 확인
-    return CCW(hull[left-1], hull[left], point) > 0
+    # 볼록 껍질이 선분인 경우 예외 처리
+    if left == len(hull) - 1:
+        return CCW(hull[0], hull[-1], point) == 0 and onLine(hull[0], hull[-1], point)
+
+    # 최종 삼각형 판정
+    # return CCW(hull[0], hull[left], point) > 0 and CCW(hull[left], hull[left+1], point) > 0 and CCW(hull[left+1], hull[0], point) > 0
+    return CCW(hull[0], hull[left], point) >= 0 and CCW(hull[left], hull[left+1], point) >= 0 and CCW(hull[left+1], hull[0], point) >= 0
